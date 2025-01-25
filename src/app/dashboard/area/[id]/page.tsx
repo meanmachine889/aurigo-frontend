399d32cd2333c5842f779429eec66f751dc6065e
itsmessc <ssaicharan2212@gmail.com>
2025-01-26 03:55:59 +0530

    refactor: simplify transaction requests layout and remove unused tabs

--- src/app/dashboard/area/[id]/page.tsx
+++ src/app/dashboard/area/[id]/page.tsx
@@ -73,36 +73,6 @@ export default function Page() {
             <TabsTrigger key={task._id} value={task._id} className="font-normal">
               {task.name}
             </TabsTrigger>
-            <TabsTrigger className="font-normal" value="2">
-              Plumbing
-            </TabsTrigger>
-            <TabsTrigger className="font-normal" value="3">
-              Furnituring
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent className="p-0" value="1">
-            <TaskPayments id="1" />
-          </TabsContent>
-          <TabsContent value="2">
-            <TaskPayments id="2" />
-          </TabsContent>
-          <TabsContent value="3">
-            <TaskPayments id="3" />
-          </TabsContent>
-        </Tabs>
-=======
-      <div className="space-y-1 p-3">
-        <h1 className="text-2xl font-normal tracking-tight">
-          Transaction Requests
-        </h1>
->>>>>>> 7763c64457d31a92995111ff9ec28d182e6196f3
-      </div>
-      <Tabs defaultValue={tasks[0]?._id || ""} className="p-3">
-        <TabsList className="grid w-fit grid-cols-3 min-h-fit mb-9">
-          {tasks.map((task) => (
-            <TabsTrigger key={task._id} value={task._id} className="font-normal">
-              {task.name}
-            </TabsTrigger>
           ))}
         </TabsList>
         {tasks.map((task) => (
